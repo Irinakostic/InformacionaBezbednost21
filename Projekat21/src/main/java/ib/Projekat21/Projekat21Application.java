0f4fa4a7ad2a0242b8f0e59beb19c353cdf7ed52
IrinaKostic <irinakostic2104@gmail.com>
2021-09-13 14:58:31 +0200

    Initial commit(resavanje problema sa git-om i eclipsom PART357)

---
+++ Projekat21/src/main/java/ib/Projekat21/Projekat21Application.java
@@ -0,0 +1,13 @@
+package ib.Projekat21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekat21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekat21Application.class, args);
+	}
+
+}
---
+++ Projekat21/src/test/java/ib/Projekat21/Projekat21ApplicationTests.java
@@ -0,0 +1,13 @@
+package ib.Projekat21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekat21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
