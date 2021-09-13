70ef60d922e47d49b95920fa2eac14e7c63a30da
IrinaKostic <irinakostic2104@gmail.com>
2021-09-13 18:40:24 +0200

    Model user authority

---
+++ Projekat21/src/main/java/ib/Projekat21/model/User.java
@@ -0,0 +1,209 @@
+package ib.Projekat21.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+
+
+@Entity
+@Table(name="User")
+public  class User implements Serializable,UserDetails{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name="certificate")
+	private String certificate;
+	
+	@Column(name = "active")
+	private boolean active;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private List<Authority> authorities = new ArrayList<Authority>();
+	
+	
+
+	public User(Long id, String email, String password, String certificate, boolean active,
+			List<Authority> authorities) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.certificate = certificate;
+		this.active = active;
+		this.authorities = authorities;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getCertificate() {
+		return certificate;
+	}
+
+
+
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+
+
+
+	public boolean isActive() {
+		return active;
+	}
+
+
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+
+
+	public List<Authority> getUserAuthorities(){
+		return authorities;
+	}
+	
+	public void setAuthorities(List<Authority> authorities) {
+	        this.authorities = authorities;
+    }
+
+ 
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+
+
+	
+
+
+	
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
