68935322c1f3378b2a2b2420804d49378a684f0b
IrinaKostic <irinakostic2104@gmail.com>
2021-09-17 13:05:16 +0200

    Revert "Mail Client dodat i repository"
    This reverts commit cc193e10c2f2c5f2550d637d9bdbc3f8a4806bec.

--- Projekat21/src/main/java/ib/Projekat21/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ib.Projekat21.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ib.Projekat21.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
